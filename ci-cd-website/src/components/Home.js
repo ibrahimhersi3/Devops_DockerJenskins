b3810e6ad969f6efa1b97fc32a1e9e3b0c2785f9
ibrahimhersi3 <ibrahim.hersi.2@gmail.com>
2023-12-28 14:35:38 +0100

    adam changes

--- ci-cd-website/src/components/Home.js
+++ ci-cd-website/src/components/Home.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Home = () => {
   return (
     <section id="home">
-      <h2>Welcome to My Portfolio</h2>
+      <h2>Welcome to My Portfolio Adam</h2>
       <p>
         I am a passionate developer with expertise in building and deploying web applications.
       </p>
