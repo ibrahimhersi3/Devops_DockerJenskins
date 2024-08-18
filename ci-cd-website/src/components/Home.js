8ddc855b696065f529e085d25721ce582febc405
ibrahimhersi3 <ibrahim.hersi@inriver.com>
2024-08-18 17:23:17 +0200

    another try

--- ci-cd-website/src/components/Home.js
+++ ci-cd-website/src/components/Home.js
@@ -1,10 +1,9 @@
 // src/components/Home.js
 import React from 'react';
 
+/* 
 const Home = () => {
   return (
-
-    /*
     <section id="home">
       <div className="cv-container">
        
@@ -13,27 +12,25 @@ const Home = () => {
           <p>Ibrahim.hersi.2@gmail.com • +46 72-233 25 22</p>
           <h3>Summary Statement</h3>
           <p>
-          I'm currently focused on advancing my career in IT operations, DevOps, and Cloud. This project ivolves it integrating Jenkins Pipelines and setting up Dockerized Jenkins environments connected to GitHub. These setups have been crucial in ensuring smooth deployments to platforms like Netlify. I've demonstrated my skills through practical projects, showcased on my portfolio website, where I discuss creating efficient CI/CD processes. For more details, please explore the "CI/CD Pipeline" section above.          </p>
+          I'm currently focused on advancing my career in IT operations, DevOps, and Cloud. This project ivolves it integrating Jenkins Pipelines and setting up Dockerized Jenkins environments connected to GitHub. These setups have been crucial in ensuring smooth deployments to platforms like Netlify. I've demonstrated my skills through practical projects, showcased on my portfolio website, where I discuss creating efficient CI/CD processes. For more details, please explore the "CI/CD Pipeline" section above.          
+          </p>
 
-          
           <h3>Work Experience</h3>
           
           <ul>
-           
             <li>
               <strong>Application Engineer</strong> <br />
-                          Inriver, Stockholm, Sweden <br />
-                          March 2022 – Present <br />
-            ● Debugged inriver applications involved analyzing customer extension code, adapting it to testing environments, and pinpointing issues using the inriver Remoting API.<br />
-            ● Identified and resolved system bugs, created dedicated branches, pull requests, and merged them into the main codebase for deployment, all while ensuring a smooth process within our CI/CD pipeline. <br />
-            ● Conducted performance tests on REST APIs, measuring various metrics such as response times, latency, and error rates to evaluate the overall performance of the API. I used performance testing tools like Postman. <br />
-            ● Used Grafana for monitoring databases, cloud services, application servers, and extensions, ensuring optimal performance, availability, and health of systems and applications. <br />
-            ● facilitated customer migrations from their on-premises databases to our inriver IPMC cloud, transferring data directly on to the Microsoft Azure cloud infrastructure. <br />
-            ● Queried and analyzed data in MSSQL to extract necessary information, and ensured consistency between application data and the database. <br />
-            ● Created technical documentation, processes, and procedures to facilitate knowledge sharing and ensure compliance with  application requirements
-                        </li><br />
+              Inriver, Stockholm, Sweden <br />
+              March 2022 – Present <br />
+              ● Debugged inriver applications involved analyzing customer extension code, adapting it to testing environments, and pinpointing issues using the inriver Remoting API.<br />
+              ● Identified and resolved system bugs, created dedicated branches, pull requests, and merged them into the main codebase for deployment, all while ensuring a smooth process within our CI/CD pipeline. <br />
+              ● Conducted performance tests on REST APIs, measuring various metrics such as response times, latency, and error rates to evaluate the overall performance of the API. I used performance testing tools like Postman. <br />
+              ● Used Grafana for monitoring databases, cloud services, application servers, and extensions, ensuring optimal performance, availability, and health of systems and applications. <br />
+              ● facilitated customer migrations from their on-premises databases to our inriver IPMC cloud, transferring data directly on to the Microsoft Azure cloud infrastructure. <br />
+              ● Queried and analyzed data in MSSQL to extract necessary information, and ensured consistency between application data and the database. <br />
+              ● Created technical documentation, processes, and procedures to facilitate knowledge sharing and ensure compliance with  application requirements
+            </li><br />
 
-           
             <li>
               <strong>Internship</strong> <br />
               qopla, Stockholm, Sweden <br />
@@ -41,7 +38,6 @@ const Home = () => {
               ● Estimating delivery times using Python and machine learning algorithms.
             </li>  <br />
               
-            
             <li>
               <strong>Visualization & Prediction Tool for Urban Water Demand - Master Thesis</strong> <br />
               Uppsala University, Uppsala, Sweden <br />
@@ -50,7 +46,6 @@ const Home = () => {
               Implemented result visualization through an interactive map using Python, OpenStreetMap, Tensorflow, and Spyder.
             </li>  <br />
 
-            
             <li>
               <strong>Collaborative Fenceless Robotics - Internship</strong> <br />
               Ericsson, Stockholm, Sweden <br />
@@ -60,7 +55,6 @@ const Home = () => {
             </li>
           </ul>
 
-          
           <h3>Education</h3>
           <p>
             <strong>Master's Degree in Computer Science</strong> <br />
@@ -74,7 +68,6 @@ const Home = () => {
             2013 - 2016
           </p>
 
-          
           <h3>Skills</h3>
           <p>
             <strong>Programming:</strong> C# .Net, Python, Javascript, HTML, CSS <br />
@@ -83,8 +76,8 @@ const Home = () => {
         </div>
       </div>
     </section>
-    */
   );
 };
+*/
 
-export default Home;
+// export default Home;
